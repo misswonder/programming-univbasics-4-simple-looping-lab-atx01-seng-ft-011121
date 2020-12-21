f5faa47452118a95639adf31a56475df7e394073
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-21 18:28:11 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,15 +16,12 @@ def loop_message_n_times(message, limit)
 end 
 
 def output_array(array)
-  count = 0 
-  new_array = []
+  count = 0
   while count < array.length do
-    new_array.push(array[count].to_s)
-    # new_array << array[count].to_s
-    count += 1 
-  end 
-  new_array
-end 
+    puts array[count]
+    count += 1
+  end
+end
 
 # def return_string_array(array)
 #   count = 0
