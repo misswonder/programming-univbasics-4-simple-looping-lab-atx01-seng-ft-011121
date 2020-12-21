700cad990825f328f3b5bd18b6d17d4d6ff34a4c
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-21 18:17:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,25 @@
 # Write your methods here
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do 
+    puts message 
+    counter += 1 
+  end 
+end 
+
+def loop_message_n_times(message, limit)
+  count = 0 
+  while count < limit do 
+    puts message
+    counter += 1 
+  end 
+end 
+
+def output_array(array)
+  count = 0 
+  while count < array.length do
+    puts array[count]
+    count += 1 
+  end 
+end 
+
